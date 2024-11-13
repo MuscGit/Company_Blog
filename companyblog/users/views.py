49e5e451807ec151e2c8f7fa3bc715805d852ac0
MuscGit <j.muscat@protonmail.com>
2024-11-13 00:23:05 +0000

    login view

--- companyblog/users/views.py
+++ companyblog/users/views.py
@@ -10,7 +10,7 @@ users = Blueprint('users',__name__)
 # crete views:
 
 # register
-@users.route('/register', method=['GET', 'POST'])
+@users.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
 
@@ -26,9 +26,26 @@ def register():
     return render_template('register.html', form = form)
 
 # login
+@users.route('login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
 
+    if form.validate_on_submit():
 
+        user = User.query.filter_by(email= form.email.data).first()
 
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)
+            flash('Log in Success!')
+
+            next = request.args.get('next') 
+
+            if next == None or not next[0]=='/':
+                next = url_for('core.index')
+
+            return redirect(next)
+        
+        return render_template('login.html')
 
 
 # logout
